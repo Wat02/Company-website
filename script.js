2524e982d69036d93ccdaab189352101d55dd142
Wat02 <121092414+Wat02@users.noreply.github.com>
2022-12-21 11:03:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+
+window.addEventListener('DOMContentLoaded', setup); 
+
+function setup() {
+    const options = {
+        rootMargin: '0px 0px -200px 0px'
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            } else {
+                return;
+            }
+        })
+    }, options);
+
+    const h1 = document.querySelector('h1');
+    observer.observe(h1);
+
+    const paras = document.querySelectorAll('p');
+    paras.forEach(p => observer.observe(p));
+
+}
+
+
+
+ 
+
+
+
+
+ 
+
